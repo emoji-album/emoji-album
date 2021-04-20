5b7dc7267c32d4f1e5c575293563a5cf13a1d3c8
Otávio Pace <otaviopp8@gmail.com>
2021-04-20 09:54:25 -0300

    :sparkles: Add reward feature (still missing 1h cooldown)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,67 @@
 use dotenv::dotenv;
+use rand::rngs::StdRng;
+use rand::seq::IteratorRandom;
+use rand::FromEntropy;
+use std::collections::HashMap;
 use std::env;
+use std::error::Error;
+use std::sync::{Arc, Mutex};
+use teloxide::{prelude::*, utils::command::BotCommand};
 
-// const EMOJIS: [char; 3] = ['😘', '🤗', '🦀'];
+const EMOJIS: [char; 3] = ['😘', '🤗', '🦀'];
 
-fn main() {
+#[derive(BotCommand)]
+#[command(rename = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "rewards with a random emoji.")]
+    Reward,
+    #[command(description = "handle a username and an age.", parse_with = "split")]
+    UsernameAndAge { username: String, age: u8 },
+}
+
+type Username = String;
+
+lazy_static::lazy_static! {
+    static ref USER_EMOJIS: Arc<Mutex<HashMap<Username, Vec<char>>>> = Arc::new(Mutex::new(HashMap::new()));
+}
+
+async fn answer(
+    cx: UpdateWithCx<AutoSend<Bot>, Message>,
+    command: Command,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match command {
+        Command::Help => cx.answer(Command::descriptions()).send().await?,
+        Command::Reward => {
+            let mut rng = StdRng::from_entropy();
+
+            let random_emoji = EMOJIS.iter().choose(&mut rng).unwrap();
+
+            cx.answer(format!("Here's a random emoji: {}", random_emoji))
+                .await?
+        }
+        Command::UsernameAndAge { username, age } => {
+            cx.answer(format!(
+                "Your username is @{} and age is {}.",
+                username, age
+            ))
+            .await?
+        }
+    };
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
     dotenv().ok();
 
-    println!("env: {}", env::var("TELOXIDE_TOKEN").unwrap());
+    teloxide::enable_logging!();
+    log::info!("Starting dices_bot...");
+
+    let bot = Bot::from_env().auto_send();
+
+    let bot_name = "Emoji Album Bot";
+    teloxide::commands_repl(bot, bot_name, answer).await;
 }
